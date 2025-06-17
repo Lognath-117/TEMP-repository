640fb849a63d18ab1b6de2594ddc94d298d7580b
Lognath .A <sonlognath0502@gmail.com>
2025-06-17 19:35:01 +0530

    Update stu_per_app.py.py

--- stu_per_app.py.py
+++ stu_per_app.py.py
@@ -7,7 +7,7 @@ Original file is located at
     https://colab.research.google.com/drive/1jdKaVuG65EKkAZIbMZOiMUpSyqwszHE4
 """
 
-!pip install streamlit
+
 import streamlit as st
 import pandas as pd
 import numpy as np
